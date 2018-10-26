1fa43dd7bc4628a2b0949c7875dde8a17cb81d6e
Shreyabeloor <shreyabeloor@gmail.com>
2018-10-26 16:01:30 +0530

    final code

--- server/src/currxhandler.py
+++ server/src/currxhandler.py
@@ -1,50 +1,41 @@
 import requests
 import json
 import datetime
+from requests import HTTPError
 
 def lambda_handler(event,context):	
+	response = dict()
+	try:
+		params_latest ={'base':event['sourcecur'],'symbols':event['dstcur'],'diff':event['difference']}	
+		latest = requests.get('http://data.fixer.io/api/latest?access_key=2e53c2379ff391cdad21359b4b83e3a2&symbols=USD,AUD,CAD,PLN,MXN,INR&format=1',params=params_latest)
+		current_cur = latest.json()
+		finalcur = event['dstcur'] 
 
-	difference = 7
-	params ={'base':event['sourcecur'],'symbols':event['dstcur']}	
-	latest = requests.get('http://data.fixer.io/api/latest?access_key=2e53c2379ff391cdad21359b4b83e3a2&symbols=USD,AUD,CAD,PLN,MXN,INR&format=1',params=params)
-	live = latest.json()
-	print(live)
-
-
-	params1 = {'base':event['sourcecur'],'symbols':event['dstcur']}
-
-	historical_date = ""
-
-	if difference == 1:
-		historical_date = datetime.date.today()-datetime.timedelta(1)
-
-	elif difference == 7:
-		historical_date = datetime.date.today()-datetime.timedelta(7)
-
-	elif difference == 30:
-		historical_date = datetime.date.today()-datetime.timedelta(30)
-
-	historical = requests.get("http://data.fixer.io/api/"+str(historical_date)+"?access_key=2e53c2379ff391cdad21359b4b83e3a2&symbols=USD,AUD,CAD,PLN,MXN&format=1",params=params1)
-	history = historical.json()
-
-	if "difference" == 1:
-		print(live['rates']['INR']-history['rates']['INR'])
-
-	elif "difference" == 7:
-		print(live['rates']['INR']-history['rates']['INR'])
-
-	elif "difference" == 30:
-		print(live['rates']['INR']-history['rates']['INR'])
-
-
-
-	print(history)
-
-		
+		params_history = {'base':event['sourcecur'],'symbols':event['dstcur']}
+		historical_date = ""
 	
-if __name__ == '__main__':
-	returnval = lambda_handler({'sourcecur':'EUR', 'dstcur':'INR'},None)
-	print(returnval)
+		historical_date = datetime.date.today()-datetime.timedelta(event['difference'])
+		
+		historical = requests.get("http://data.fixer.io/api/"+str(historical_date)+"?access_key=2e53c2379ff391cdad21359b4b83e3a2&symbols=USD,AUD,CAD,PLN,MXN&format=1",params=params_history)
+		history = historical.json()
 
 
+		if current_cur['success'] == True:
+        		response["Success"]= "true" 
+        		response["Currency"] = "{:.2f}".format(current_cur['rates'][finalcur])
+        		difference = "{:.2f}".format(current_cur['rates'][finalcur]-history['rates'][finalcur])
+        		if float(difference) < 0:
+        			response['difference'] =  difference
+        		if float(difference) > 0: 
+        			response['difference'] = "+" + difference
+        
+
+		elif current_cur['success'] == False:
+				response["status"]= "false"
+				response["error"] = "Invalid data"
+	except requests.exceptions.HTTPError as e:
+		response["status"]= "false"	 
+		response["error"] = "Internal server error"
+	return (response)
+    
 	
